0fbb2f1377272441b264e96f1e37005658fc8937
solo samurai <ahmed_abdikani@hotmail.com>
2024-01-03 05:32:13 +0300

    :sparkles: add icons

---
+++ src/assets/icons/ChevronLeft.svelte
@@ -0,0 +1,7 @@
+<svg viewBox="0 0 24 24" fill="currentColor" height="2em" width="2em">
+	<path
+		stroke-linecap="round"
+		stroke-linejoin="round"
+		d="M15.41 16.58L10.83 12l4.58-4.59L14 6l-6 6 6 6 1.41-1.42z"
+	/>
+</svg>
---
+++ src/assets/icons/ChevronRight.svelte
@@ -0,0 +1,7 @@
+<svg viewBox="0 0 24 24" fill="currentColor" height="2em" width="2em">
+	<path
+		stroke-linecap="round"
+		stroke-linejoin="round"
+		d="M8.59 16.58L13.17 12 8.59 7.41 10 6l6 6-6 6-1.41-1.42z"
+	/>
+</svg>
