c91db3ee2888f86fae7a5162c3db02ea3375e9f5
solo samurai <ahmed_abdikani@hotmail.com>
2024-01-03 05:32:48 +0300

    :sparkles: create pagination component

---
+++ src/components/Pagination.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import ChevronLeft from '../assets/icons/ChevronLeft.svelte';
+	import ChevronRight from '../assets/icons/ChevronRight.svelte';
+
+	export let page = 0;
+	export let onPageChange = (page: number) => {};
+</script>
+
+<div class="flex gap-5 items-center my-8 mr-10">
+	<button
+		class="border border-white/50 rounded-full aspect-square h-16 grid place-content-center"
+		on:click={() => onPageChange(page - 1)}
+	>
+		<ChevronLeft />
+	</button>
+	<button
+		class="border border-white/50 rounded-full aspect-square h-16 grid place-content-center"
+		on:click={() => onPageChange(page + 1)}
+	>
+		<ChevronRight />
+	</button>
+
+	<hr class="border border-white/50 flex-1" />
+
+	<p class="text-6xl">
+		<!-- 01 -->
+		{(page + 1).toString().padStart(2, '0')}
+	</p>
+</div>
