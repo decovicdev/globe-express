8fe4fb6503225647829dfcdbb7146bd8361baadb
solo samurai <ahmed_abdikani@hotmail.com>
2024-01-14 09:44:01 +0300

    :sparkles: add icons

---
+++ src/assets/icons/Menu.svelte
@@ -0,0 +1,6 @@
+<svg fill="none" viewBox="0 0 24 24" height="2em" width="2em">
+	<path
+		fill="currentColor"
+		d="M4 6a1 1 0 011-1h14a1 1 0 110 2H5a1 1 0 01-1-1zM4 18a1 1 0 011-1h14a1 1 0 110 2H5a1 1 0 01-1-1zM11 11a1 1 0 100 2h8a1 1 0 100-2h-8z"
+	/>
+</svg>
